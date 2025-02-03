33fbe784e92201ca3690df54afead583738111b1
zjones7 <zjones7@mail.pima.edu>
2025-02-03 14:13:40 -0700

    Update cis129_lab02_shortStory.py
    mod 2 lab

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,6 @@
-print('Hello, World!')
+print('Hello, class')
+# this is the first comment
+print('This is for my cis class')
+print('The class is cis 129')
+# a short story
+print('I'm taking it in spring')
